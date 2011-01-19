41971e2624f34e57dd58504e22c9120ee5786522
AK <dnamax@gmail.com>
2011-01-18 19:45:44 -0800

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '09814a94b5b4150dae02b3e9e8a2c6ba8139dcd4c28ae345b6ff5dc0d9d59fb77b6cace9920aa0ad8e68a17731da2d7a8ff220f1614f9100f7dd0ca035701ce4'
